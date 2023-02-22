0d9c0dba20932bffe5c51638a3cc49f55f5e54fd
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-22 10:36:14 +0100

    2

--- app/src/main/java/com/example/mejora/Login.kt
+++ app/src/main/java/com/example/mejora/Login.kt
@@ -1,11 +1,73 @@
 package com.example.mejora
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.example.mejora.databinding.ActivityMainBinding
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setup()
     }
+    private fun setup() {
+        title = "Login"
+
+        binding.registro.setOnClickListener {
+            if (binding.loginedit.text.isNotEmpty() && binding.password.text.isNotEmpty()) {
+
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        binding.loginedit.text.toString(),
+                        binding.password.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        }else{
+                            showAlert()
+                        }
+                    }
+            }
+        }
+        binding.loginbutton.setOnClickListener {
+            if (binding.loginedit.text.isNotEmpty() && binding.password.text.isNotEmpty()) {
+
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(
+                        binding.loginedit.text.toString(),
+                        binding.password.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        }else{
+                            showAlert()
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email:String,provider: ProviderType){
+        val homeIntent: Intent = Intent(this, prueba::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
 }
--- app/src/main/java/com/example/mejora/prueba.kt
+++ app/src/main/java/com/example/mejora/prueba.kt
@@ -2,10 +2,34 @@ package com.example.mejora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mejora.databinding.ActivityPruebaBinding
+import com.google.firebase.auth.FirebaseAuth
 
+enum  class ProviderType{
+    BASIC
+}
 class prueba : AppCompatActivity() {
+    private lateinit var binding : ActivityPruebaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityPruebaBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_prueba)
+        setContentView(binding.root)
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email:String , provider: String){
+
+        title = "Inicio"
+        binding.emailTextView.text = email
+        binding.providerTextView.text = provider
+
+        binding.cerrarSesion.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
